40bf2b4390ad75ef884187ccf7cb0a0310a1e233
Grant Bartlett <grant@smswmedia.com>
2014-04-29 17:16:39 +0100

    SFM Loves you.

--- index.php
+++ index.php
@@ -11,6 +11,40 @@
 <!-- Eight/Four Columns :Articles and Sidebar -->
 <?php include_once('_content/articles.php');?>
 
+<table class="container">
+    <tr>
+        <td>
+            <table class="row">
+                <tr>
+                    <td>
+                        <table class="twelve columns">
+                            <tr>
+                                <td class="panel weekly" style="background: #ffffff;">
+                                    <div style="margin: 30px 20px;text-align: center;">
+                                        <span style="font-size: 19px;color: #000;">
+                                            <multiline>
+                                                Overheard
+                                            </multiline>
+                                        </span>
+                                        <p style="text-align: center;"> What you've been saying about SFM...</p>
+                                    </div>
+                                    <div style="font-size: 18px;line-height: 1.7;font-style: italic;margin: 30px 20px;text-align: center;">
+                                        <span>
+                                           <strong>"<a href="http://instagram.com/p/nKgQ0wKj4d/">@marchaydenphoto</a></strong> loving your work :)"<br>
+                                            <strong>"<a href="http://instagram.com/p/nU-TQ0qj6H/">@mari14bella</a></strong> Love the blog ! Good job xx"
+                                        </span>
+                                    </div>
+                                </td>
+                                <td class="expander"></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
 <?php include_once('_content/social.php');?>
 <?php include_once('_content/unsubscribe.php');?>
 <?php include_once('_includes/footer.php');?>
